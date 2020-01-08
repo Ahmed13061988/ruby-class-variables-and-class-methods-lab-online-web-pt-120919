3124493bb7fcbcb7c12c1a545eacd57b6a7bc400
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-08 23:18:01 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+    @@count +=1
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@artists << artist
+    @@genres << genre
+  end
+  def self.count
+    @@count
+  end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.genre_count
+    library = {}
+    @@genres.each do |genre|
+      if library.key?(genre)
+        library[genre] += 1 
+      else
+        library[genre] = 1 
+      end
+    end
+    library
+  end
